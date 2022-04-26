9c84f11e230ac8339ec42a8cdac1c7bbddb753fa
Akashdeep Srivastava <asrivastava636@gmail.com>
2022-04-26 09:19:04 +0200

    Update render-build.sh

--- bin/render-build.sh
+++ bin/render-build.sh
@@ -5,6 +5,6 @@ set -o errexit
 bundle install
 bundle exec rails assets:precompile
 bundle exec rails assets:clean
-bundle exec rails db:migrate
-bundle exec rails db:seed
-bundle exec rake spree_sample:load
+#bundle exec rails db:migrate
+#bundle exec rails db:seed
+#bundle exec rake spree_sample:load
