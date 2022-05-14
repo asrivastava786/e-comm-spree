be20204131f4b5590f263af34429c1a0d9f77f90
Akashdeep Srivastava <asrivastava636@gmail.com>
2022-05-14 14:14:04 +0200

    Update render-build.sh

--- bin/render-build.sh
+++ bin/render-build.sh
@@ -4,7 +4,7 @@ set -o errexit
 
 
 bundle install
-bundle exec rails g spree_analytics_trackers:install
+#bundle exec rails g spree_analytics_trackers:install
 bundle exec rails assets:precompile
 bundle exec rails assets:clean
 bundle exec rails db:migrate
